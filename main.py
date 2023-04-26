387ddb896c591c07562dd5faec93e9aafcd1bf37
Farnaz <farnazfawadhasan@gmail.com>
2023-04-26 22:51:10 +0600

    last commit on 26/4/23

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Parent Class
+
 class User():
     def __init__(self, name, age, gender):
         self.name = name
@@ -5,8 +7,45 @@ class User():
         self.gender = gender
 
     def show_details(self):
-        print("Delete this Line.")
-        print("This line needs to be modified..........")
+        print("Personal Details")
+        print("")
+        print("Name ", self.name)
+        print("Age ", self.age)
+        print("Gender ", self.gender)
+
+#======================================
+# johan = User("Johan", 21, "Male")
+# johan.show_details()
+
+#Child Class
 
 
+class Bank(User):
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+        self.balance = 0
 
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Account balance has been updated: ", self.balance)
+
+    def withdraw(self, amount):
+        self.amount= amount
+        if self.amount > self.balance:
+            print("Insufficent fund || Available Balance: ", self.balance)
+
+        else:
+            self.balance = self.balance - self.amount
+            print("Account balance has been updated || Available Balance:" , self.balance)
+
+    def view_balance(self):
+        self.show_details()
+        print("Account Balance is ", self.balance)
+
+
+johan = Bank("Johan", 20, "Male")
+print(johan.age)
+johan.deposit(1000)
+johan.withdraw(200)
+johan.show_details()
